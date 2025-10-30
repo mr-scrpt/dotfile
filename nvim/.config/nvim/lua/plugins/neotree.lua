4ec03ab4a0947491e27bb893636c1629d53dc3b3
mr <mr.scrpt@gmail.com>
2025-10-30 13:12:36 +0200

    feat: correct fav neo tree

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -5,12 +5,10 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   cmd = "Neotree",
 
-  -- V-- ДОБАВЛЯЕМ ЗАВИСИМОСТИ --V
   dependencies = {
     "MagicDuck/grug-far.nvim",
-    "johmsalas/text-case.nvim", -- (Опциональная зависимость grug-far из твоего старого конфига)
+    "johmsalas/text-case.nvim",
   },
-  -- ^-- ДОБАВЛЯЕМ ЗАВИСИМОСТИ --^
 
   keys = {
     {
@@ -69,7 +67,6 @@ return {
     sources = { "filesystem", "buffers", "git_status", "favorites" },
     popup_border_style = "rounded",
 
-    -- V-- ДОБАВЛЯЕМ КОМАНДЫ --V
     commands = {
       grug_far_replace = Utils.grug_far_replace,
       grug_far_replace_visual = Utils.grug_far_replace_visual,
@@ -77,18 +74,16 @@ return {
       remove_from_favorites = FavoritesCommands.remove_from_favorites,
       toggle_favorite = FavoritesCommands.toggle_favorite,
     },
-    -- ^-- ДОБАВЛЯЕМ КОМАНДЫ --^
 
     window = {
       mappings = {
-        ["Y"] = Utils.copy_path, -- Наша функция с выбором
-        ["R"] = Utils.grug_far_open, -- Новый маппинг для grug-far
-        ["S"] = "add_to_favorites", -- Добавить в избранное (S = Star/Save)
-        ["D"] = "remove_from_favorites", -- Удалить из избранного (D = Delete)
+        ["Y"] = Utils.copy_path,
+        ["R"] = Utils.grug_far_open,
+        ["S"] = "add_to_favorites",
+        ["D"] = "remove_from_favorites",
       },
     },
 
-    -- Source selector для переключения между вкладками
     source_selector = {
       winbar = true,
       content_layout = "center",
@@ -100,31 +95,31 @@ return {
       },
     },
 
-    -- Добавляем кастомный компонент для индикации избранного
-    default_component_configs = {
-      indent = {
-        with_expanders = true,
-        expander_collapsed = "",
-        expander_expanded = "",
-        expander_highlight = "NeoTreeExpander",
-      },
-      icon = {
-        folder_closed = "",
-        folder_open = "",
-        folder_empty = "󰜌",
-        default = "*",
-        highlight = "NeoTreeFileIcon",
-      },
-    },
-
-    -- Настройка для filesystem
     filesystem = {
       bind_to_cwd = false,
       follow_current_file = { enabled = true },
       use_libuv_file_watcher = true,
+      components = {
+        favorite_indicator = require("custom.neotree-favorites.component"),
+      },
+      renderers = {
+        directory = {
+          { "indent" },
+          { "icon" },
+          { "current_filter" },
+          { "name" },
+          { "favorite_indicator" },
+        },
+        file = {
+          { "indent" },
+          { "icon" },
+          { "name", use_git_status_colors = true },
+          { "favorite_indicator" },
+          { "git_status" },
+        },
+      },
     },
 
-    -- Конфигурация для favorites source
     favorites = {
       bind_to_cwd = false,
       follow_current_file = { enabled = false },
